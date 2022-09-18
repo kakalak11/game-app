e7b291d301242276057908174e988182ae6ad543
kakalak11 <92971472+kakalak11@users.noreply.github.com>
2022-09-18 13:25:46 +0700

    - update logic steer ship

--- assets/scripts/core/ShipControl.js
+++ assets/scripts/core/ShipControl.js
@@ -4,10 +4,17 @@ cc.Class({
     properties: {
         bulletPrefab: cc.Prefab,
         bulletHolder: cc.Node,
+
+        steerLeft: cc.SpriteFrame,
+        steerRight: cc.SpriteFrame,
+        steerForward: cc.SpriteFrame,
     },
 
     onLoad() {
         cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+
+        this._sprite = this.node.getComponentInChildren(cc.Sprite);
     },
 
     onKeyUp(event) {
@@ -16,7 +23,22 @@ cc.Class({
             bullet.parent = this.bulletHolder;
             bullet.position = this.node.getPosition();
         }
+
+        const isReturnToIdle = event.keyCode === cc.macro.KEY.d || event.keyCode === cc.macro.KEY.a;
+        if (isReturnToIdle) this._sprite.spriteFrame = this.steerForward;
+        cc.warn(isReturnToIdle);
     },
 
-    
+    onKeyDown(event) {
+        if (event.keyCode == cc.macro.KEY.a) {
+            this._steerHorizontal({ isLeft: true });
+        } else if (event.keyCode == cc.macro.KEY.d) {
+            this._steerHorizontal({ isLeft: false });
+        }
+    },
+
+    _steerHorizontal({ isLeft = true }) {
+        if (isLeft) this._sprite.spriteFrame = this.steerLeft;
+        else this._sprite.spriteFrame = this.steerRight;
+    }
 });
--- library/imports/a4/a4e113e3-20c1-4c4f-83ac-6bbd20db8053.js
+++ library/imports/a4/a4e113e3-20c1-4c4f-83ac-6bbd20db8053.js
@@ -9,11 +9,18 @@ cc.Class({
 
     properties: {
         bulletPrefab: cc.Prefab,
-        bulletHolder: cc.Node
+        bulletHolder: cc.Node,
+
+        steerLeft: cc.SpriteFrame,
+        steerRight: cc.SpriteFrame,
+        steerForward: cc.SpriteFrame
     },
 
     onLoad: function onLoad() {
         cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+
+        this._sprite = this.node.getComponentInChildren(cc.Sprite);
     },
     onKeyUp: function onKeyUp(event) {
         if (event.keyCode == cc.macro.KEY.space) {
@@ -21,6 +28,23 @@ cc.Class({
             bullet.parent = this.bulletHolder;
             bullet.position = this.node.getPosition();
         }
+
+        var isReturnToIdle = event.keyCode === cc.macro.KEY.d || event.keyCode === cc.macro.KEY.a;
+        if (isReturnToIdle) this._sprite.spriteFrame = this.steerForward;
+        cc.warn(isReturnToIdle);
+    },
+    onKeyDown: function onKeyDown(event) {
+        if (event.keyCode == cc.macro.KEY.a) {
+            this._steerHorizontal({ isLeft: true });
+        } else if (event.keyCode == cc.macro.KEY.d) {
+            this._steerHorizontal({ isLeft: false });
+        }
+    },
+    _steerHorizontal: function _steerHorizontal(_ref) {
+        var _ref$isLeft = _ref.isLeft,
+            isLeft = _ref$isLeft === undefined ? true : _ref$isLeft;
+
+        if (isLeft) this._sprite.spriteFrame = this.steerLeft;else this._sprite.spriteFrame = this.steerRight;
     }
 });
 
